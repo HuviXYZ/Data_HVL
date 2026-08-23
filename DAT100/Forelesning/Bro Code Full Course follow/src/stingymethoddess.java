4df9ff4403bfa42b32bbb7e128ba3dad1cead223
HuviXYZ <h.nygjerde2@gmail.com>
2026-08-23 02:33:26 +0200

    Update stingymethoddess.java

--- DAT100/Forelesning/Bro Code Full Course follow/src/stingymethoddess.java
+++ DAT100/Forelesning/Bro Code Full Course follow/src/stingymethoddess.java
@@ -1,2 +1,22 @@
+import java.util.Locale;
+
 public class stingymethoddess {
+    public static void main(String[] args) {
+
+        String name = "HåVard ";
+        //boolean result = name.equalsIgnoreCase("Håvard");
+        //int result = name.length();
+        //char result = name.charAt(2);
+        //int result = name.indexOf("D");
+        //boolean result = name.isBlank();
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.strip();
+        //String result = name.replace("H", "G");
+        String result = name.repeat(4);
+
+
+
+        System.out.println(result);
+    }
 }
