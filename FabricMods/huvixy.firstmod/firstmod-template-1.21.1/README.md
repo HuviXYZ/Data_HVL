e31e75c5f8a0ed51b7000967d39f0edb026d1f34
HuviXYZ <h.nygjerde2@gmail.com>
2026-08-25 09:25:36 +0200

    fix
    fix

--- FabricMods/huvixy.firstmod/firstmod-template-1.21.1/README.md
+++ FabricMods/huvixy.firstmod/firstmod-template-1.21.1/README.md
@@ -1,4 +1,4 @@
-# AppleKiwi
+# FirstMod
 
 ## Setup
 
--- FabricMods/huvixy.firstmod/firstmod-template-1.21.1/src/main/java/huvixy/firsmod/FirstMod.java
+++ FabricMods/huvixy.firstmod/firstmod-template-1.21.1/src/main/java/huvixy/firsmod/FirstMod.java
@@ -1,14 +1,14 @@
-package com.example;
+package huvixy.firsmod;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.resources.Identifier;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AppleKiwi implements ModInitializer {
-	public static final String MOD_ID = "applekiwi";
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -25,6 +25,6 @@ public class AppleKiwi implements ModInitializer {
 	}
 
 	public static Identifier id(String path) {
-		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+		return Identifier.of(MOD_ID, path);
 	}
 }
